e58305a1e652e7a877e3ba591dec79fd10029a47
Flarpz <marcuskarrm@gmail.com>
2017-05-09 00:10:09 +0200

    added-icons

--- project-v5/dashboard.php
+++ project-v5/dashboard.php
@@ -121,8 +121,13 @@ BODY;
 
 $end = <<<END
   <!-- End page content -->
-</div>
-
+</div style>
+<footer style="text-align: center; margin-top: 25px;">
+  <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+  <div>Icons made by <a href="http://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+  <div>Icons made by <a href="http://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+  <div>Icons made by <a href="http://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+</footer>
 <script src="./media/js/sidebar.js"></script>
 </body>
 </html>
--- project-v5/index.php
+++ project-v5/index.php
@@ -1,4 +1,33 @@
 <?php
+
+function typeOfImage($value){
+  $returnValue = "";
+  switch ($value){
+    case 1:
+       $returnValue = "media/img/monitor.png"; //TV
+      break;
+    case 2:
+       $returnValue = "media/img/desk-lamp.png"; // Lamp
+      break;
+    case 3: 
+       $returnValue = "media/img/freezer.png"; // Frezzer
+      break;
+    case 4:
+       $returnValue = "media/img/refrigerator.png"; // Refrigerator
+      break;
+    case 5:
+       $returnValue = "media/img/megaphone.png"; // Speaker
+      break;
+    case 6:
+      $returnValue = "media/img/thermometer.png"; // Temp
+      break;
+    default:
+       $returnValue = "media/img/monitor.png";
+       break; 
+  }
+  return $returnValue;
+ }
+
 require_once('db.php');
 //checks if you're logged in or not
 
@@ -16,9 +45,6 @@ echo $body;
 <!-- <a href="#" class="w3-bar-item w3-button w3-padding"><i class="{$icon}"></i>  {$row->room_name}</a> -->
 <?php
 $indexContent = <<<END
-  <header class="w3-container" style="padding-top:22px">
-    <h5><b><i class="fa fa-dashboard"></i> My Dashboard</b></h5>
-  </header>
   <div class="w3-row w3-container">
 END;
 $query = <<<END
@@ -29,21 +55,21 @@ END;
 $res = $mysqli->query($query);
 if ($res->num_rows > 0) {
  while ($row = $res->fetch_object()) {
-  $icon = typeOfIcon($row->type_id);
+  $img = typeOfImage($row->type_id);
   $indexContent .= <<<CONTENT
 <div class="w3-third w3-container">
-  <div class="w3-card-4" style="margin-top: 25px;">
-    <img src="media/img/Lamp.jpg" style="width:100%">
+  <div class="w3-card-4" style="margin-top: 25px; background: white;">
+    <div style="text-align: center;"><h6 style="">{$row->room_name}</h6></div>
+    <img src="{$img}" style="margin-left: 5%; width: 90%;">
     <div class="w3-container w3-center">
       <div class="w3-container w3-center row" style="height: 35px">
-        <div class="row" style="height: inherit; margin-top: 6px;">
-        <h4 style="">{$row->room_name}</h4>
-        <h4 style="margin-left:10px;">|  {$row->description}  |</h4>
+        <div class="row" style="height: inherit; margin-top: 10px;">
+        <h6 style="margin-left:10px;">|  {$row->description}  |</h6>
 CONTENT;
         if(isset($_SESSION['super_user'])){
           $indexContent .= <<<CONTENT
           <a href="remove_device.php?id={$row->device_id}" onclick="return confirm('Are you sure you want to delete the device?')">
-            <h4 style="color: black; margin-left:10px">X</h4>
+            <h6 style="color: black; margin-left:10px">X</h6>
           </a>
 CONTENT;
         }
